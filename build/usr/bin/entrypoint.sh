50d01ec51d8481c6f5717e3a1ac10386893156b0
Jason <git-admin@casjaysdev.com>
2021-12-14 16:44:32 -0500

    🗃 Modified: build/Dockerfile build/usr/bin/entrypoint.sh  🗃

--- build/usr/bin/entrypoint.sh
+++ build/usr/bin/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+case "$1" in
+--health)
+  if ps aux | grep -v grep | grep -q "[d]ict"; then
+    exit 0
+  else
+    exit 1
+  fi
+  ;;
+esac
+
 [[ -f "/config/dict.conf" ]] &&
   cp -Rf "/config/dict.conf" "/etc/dictd/dict.conf" ||
   cp -Rf "/etc/dictd/dict.conf" "/config/dict.conf"
